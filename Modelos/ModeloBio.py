99e570252fbc24463f51f4f143bb17ba799f8b3d
Polaryti <45695029+Polaryti@users.noreply.github.com>
2020-02-26 11:02:04 +0100

    Last

--- Modelos/ModeloBio.py
+++ Modelos/ModeloBio.py
@@ -11,17 +11,16 @@ procesador = proc.ProcesadorBio("C:\\Users\\mario\\Documents\\GitHub\\TensorFlow
 
 # 2: Creación de la red neuronal
 model = keras.Sequential([
-    keras.layers.Dense(2048, activation='relu'),
-    keras.layers.Dense(1024, activation='relu'),
-    keras.layers.Dense(512, activation='relu'),
-    keras.layers.Dense(256, activation='relu'),
-    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(32, activation='tanh'),
+    keras.layers.Dense(64, activation='relu'),
     keras.layers.Dense(64, activation='relu'),
     keras.layers.Dense(2, activation='softmax')
 ])
 
 # 3: Función de optimización y metrica a optimizar
-model.compile(optimizer='rmsprop',
+model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
--- Preprocesamiento/PreprocesamientoBio.py
+++ Preprocesamiento/PreprocesamientoBio.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 import numpy as np
-from numpy import genfromtxt
 import pandas as pd
 
 
